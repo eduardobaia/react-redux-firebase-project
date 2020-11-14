bc3f6553a97706148cbd34b0a0db5c935f13e50d
eduardobaia <eduardo_baia@msn.com>
2020-11-14 16:18:59 +0000

    project with firebase structure and menus

---
+++ src/components/layout/SignedOutLinks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export const SignedOutLinks = () => {
+    return (
+        <ul className="right">
+            <li><NavLink to="/">Signup</NavLink></li>
+            <li><NavLink to="/">Login</NavLink></li>
+        </ul>
+    )
+}
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export const ProjectList = () => {
+    return (
+        <div className="project-list section">
+            <div className="card z-depth-0 project-sumary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by eduardo </p>
+                    <p className="grey-text"> 3rd september, 2am</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-sumary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by eduardo </p>
+                    <p className="grey-text"> 3rd september, 2am</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-sumary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by eduardo </p>
+                    <p className="grey-text"> 3rd september, 2am</p>
+                </div>
+            </div>
+        </div>
+
+
+    )
+}
