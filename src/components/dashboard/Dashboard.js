caa7ef47f9d1f51bcf47236517a9e3a5203fde6a
Eduardo Baia <eduardo_baia@msn.com>
2021-02-20 12:40:43 +0000

    add notification bar and funcitons on firebase

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import ProjectList from '../projects/ProjectList'
-import { Notifications } from './Notifications'
+
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
+import Notifications from './Notifications'
 
 class Dashboard extends Component {
+
     render() {
 
         // console.log(this.props);
@@ -21,7 +23,7 @@ class Dashboard extends Component {
                         <ProjectList projects={projects} />
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications />
+                       <Notifications />
                     </div>
                 </div>
             </div>
