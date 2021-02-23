596795d46ad3424b2a5e0a153afdb5c7ca450e40
Eduardo Baia <eduardo_baia@msn.com>
2021-02-23 20:27:15 +0000

    fixing error whenn back in date

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -6,7 +6,7 @@ export const ProjectSummary = ({project}) => {
         <div className="card-content grey-text text-darken-3">
             <span className="card-title">{project.title}</span>
             <p>Posted by {project.authorFirstName} {project.authorLastName} </p>
-            <p className="grey-text"> {moment(project.createdAt.toDate().toString()).calendar()}</p>
+            <p className="grey-text"> {moment(project.createdAt && project.createdAt.toDate().toString()).calendar()}</p>
         </div>
     </div>
     )
